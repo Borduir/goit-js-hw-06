7cb8429cdba60eea8db52eebf967b4259b1b3d3a
Denis Smolianyi <denis.smolyanoj@gmail.com>
2022-09-20 21:39:26 +0300

    task 9 completed

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector("body");
+const changeColorButton = body.querySelector(".change-color");
+const color = body.querySelector(".color");
+changeColorButton.addEventListener("click", () => {
+  body.style.backgroundColor = getRandomHexColor();
+  color.textContent = getRandomHexColor();
+});
