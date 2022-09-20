c61512b37b5c2ab23c9b2d0311542a5d3dd59e32
Denis Smolianyi <denis.smolyanoj@gmail.com>
2022-09-20 16:15:45 +0300

    2 task completed

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients')
+
+ingredients.forEach(ingredient => {
+  const ingredientsItem = document.createElement('li')
+  ingredientsItem.textContent = ingredient
+  ingredientsItem.classList.add('item')
+  ingredientsList.append(ingredientsItem)
+  })
+
+console.log(ingredientsList)
+
