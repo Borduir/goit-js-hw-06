0f6b143cfa7c49b92b900b53cb1456d29012e430
Denis Smolianyi <denis.smolyanoj@gmail.com>
2022-09-20 18:04:07 +0300

    task 3 completed

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -12,3 +12,29 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const gallery = document.querySelector('.gallery')
+let newList = []
+// const imageList = images.map(element => {
+//   const imageEl = document.createElement('img')
+//   imageEl.setAttribute("src",`${element.url}`)
+//   imageEl.setAttribute("alt", `${element.alt}`)
+//   console.log(imageEl)
+//   newList.push(imageEl)
+// })
+// console.log(newList)
+// gallery.append(...newList)
+
+const imageList = images.map(element => {
+  const imageEl = document.createElement('li')
+  const markup = `<img src="${element.url}" alt=${element.alt} width=300px height=300px></img>`
+  imageEl.insertAdjacentHTML("beforeend", markup);
+  imageEl.style.display = "flex"
+  imageEl.style.margin = "5vh 30vw"
+  imageEl.style.border = "5px solid blue"
+  console.log(imageEl)
+  newList.push(imageEl)
+})
+
+console.log(newList)
+gallery.append(...newList)
