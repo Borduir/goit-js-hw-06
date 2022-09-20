f7233ac9f03766acb13df1e4ac48f74043dca261
Denis Smolianyi <denis.smolyanoj@gmail.com>
2022-09-20 21:27:47 +0300

    task 8 completed

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,5 +1,5 @@
 const input = document.querySelector('input')
-input.addEventListener("keyup", event => {
+input.addEventListener("blur", event => {
     if (input.value.length === Number(input.getAttribute('data-length'))) {
         input.classList.add('valid')
         input.classList.remove('invalid')
